a5dc4ece7208b35f0ac4d0480b686b25c06a02ca
ruben <ruben>
2015-06-06 11:44:16 +0200

    Arregado el problema al pasar a mayúsculas.

--- tools/capitalize/capitalize.py
+++ tools/capitalize/capitalize.py
@@ -11,18 +11,36 @@
 
 # Python version: >= 2.7 & <3
 import os
+import re
 import signal
 import sys
 
 
 FILE_NAME = "lista trabajos hip-hop español.csv"
+FIRST_CHAR_PATTERN = re.compile("(\"[a-zA-Z])", re.UNICODE)
+STR_EXCEPTIONS = ['BBoy', 'BCM', 'BLK', 'BLS', 'BZN', 'BeatKraken', 'CD',
+                  'CHR', 'CLS', 'CPV', 'CQD', 'DCP', 'DG', 'DJ', 'DLux', 'DNI',
+                  'DPC', 'DVD', 'DVTZ', 'DaCream', 'DobleJota', 'EP', 'EUPMC',
+                  'ElSucio', 'FBeats', 'FJ Ramos', 'FK Crew', 'Ferran MDE',
+                  'GT Castellano', 'GranPurismo', 'HC', 'HDC', 'HR', 'IFE',
+                  'JHT', 'JML', 'JNK', 'JP', 'JPelirrojo', 'JotaJota', 'KAOS',
+                  'KFS & Ochoa', 'LG', 'LJDA', 'LP', 'LSK', 'LaFé', 'LaOdysea',
+                  'MC', 'MCB', 'MDE', 'NH', 'NeOne', 'NomadaSquaD',
+                  'NonDuermas', 'Nora LaRock', 'PFG', 'PGP', 'RCA', 'RNE3',
+                  'RdM', 'SDJ', 'SDave', 'SFDK', 'SH', 'SHN', 'SKL69', 'Sr',
+                  'Sr.', 'TCap', 'TDK', 'THX', 'TNGHT', 'TV', 'URS', 'VPS',
+                  'VSK', 'VV.AA.', 'XChent', 'XL', 'XXL', 'XXX', 'ZNP', 'vs',
+                  'yOSEguiré']
+ALPHA_MATCH = re.compile("[a-zA-Z \"\(\)]*")
 
+
+# STR_EXCEPTIONS = ['JPelirrojo']
 
 def signal_handler(signal, frame):
     """
     signal_handler(signal, frame)
         Sets handlers for asynchronous events.
-    Arguments:
+    Argumentos:
         - signal: Signal number.
         - frame:  Current stack frame.
     """
@@ -43,6 +61,58 @@ def __clear_screen():
         os.system('clear')
 
 
+def __repl_str_exc(line):
+    """
+    __repl_str_ex
+        Reemplaza las excepciones de cadena en una línea
+    Argumentos:
+        - line (string): Línea
+    """
+
+    lline = line.split(',')
+    result_line = ''
+
+    for field in lline:
+        alpha_match = ALPHA_MATCH.search(field)
+        if(alpha_match):
+            if(alpha_match.group(0) != ''):
+                lfield = field.split()
+                for word in lfield:
+                    for ex in STR_EXCEPTIONS:
+                        if(ex.lower() in word.lower()):
+                            clean_word = re.search("[a-z]+", word.lower())
+                            if(clean_word.group(0) == ex.lower()):
+                                field = field.replace(word, ex)
+        # Reassembling the string
+        result_line = '{0},{1}'.format(result_line, field)
+
+    return result_line.lstrip(',')
+
+
+def __repl_func(m):
+    """
+    __repl_func(m)
+        Procesa una expresión regular.
+    Argumentos:
+        - m (regex): expresión regular
+    """
+
+    return m.group(1).upper()
+
+
+def __capitalize_line(line):
+    """
+    __capitalize_line()
+        Pasa a mayúsculas una línea
+    Argumentos:
+        - line (string): Línea
+    """
+
+    capitalized_line = re.sub(FIRST_CHAR_PATTERN, __repl_func, line)
+
+    return (capitalized_line)
+
+
 def __capitalize_lines():
     """
     __capitalize_lines()
@@ -56,7 +126,9 @@ def __capitalize_lines():
     fo = open(output_file, 'w+')
 
     for line in f:
-        fo.write(line.title())
+        capitalized_line = __capitalize_line(line)
+        formatted_line = __repl_str_exc(capitalized_line)
+        fo.write('{0}\n'.format(formatted_line))
 
     f.close()
     fo.close()
